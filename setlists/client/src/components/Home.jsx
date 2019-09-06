2c3fdcb4770ba6b617dc983ca87b3a9cc7625468
Francine Altman <francinekline@gmail.com>
2019-09-06 15:18:52 -0400

    third meta tag fix

--- setlists/client/src/components/Home.jsx
+++ setlists/client/src/components/Home.jsx
@@ -6,10 +6,10 @@ import Typewriter from '../assets/typewriter_nw_700.png';
 export default function Home() {
   return (
     <div className="homepage">
-      <p className="line-1 anim-typewriter">Setlist Generator...</p>
+      <p className="line-1 anim-typewriter">Setlist Generator</p>
       <div className="home-links">
         <Link to='/generate-setlist'>
-          <button className="home-generate-a-list-button" onClick={() => { }}>Generate a New Setlist</button></Link>
+          <button className="home-generate-a-list-button" onClick={() => { }}>New Setlist</button></Link>
         <br />
         {/* <Link to='/setlist-archives'>
           <button className="home-go-to-archives-button">Setlist Archives</button>
